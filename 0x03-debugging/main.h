fe81526b0c381e23ef68525ba7d9d39108d584bb
Dannyg2244 <ikuyajolugbenga@gmail.com>
2022-09-14 11:45:53 -0700

    task 0

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+/**
+ * positive or negative - checked positive or negative
+ * @i: the number to be checked is 0, 1, -1
+ * Return: 0, 1, -1 accordingly
+ */
+
+void positive_or_negative(int i);
