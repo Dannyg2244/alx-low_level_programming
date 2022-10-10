d8453a92f6edc6828f0da2241b7149046c70a245
Dannyg2244 <ikuyajolugbenga@gmail.com>
2022-10-10 08:03:46 -0700

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * init_dog - initializes a dog
+ * @d: addresses of the dog
+ * @name: name of the dog
+ * @owner: owner of the dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
